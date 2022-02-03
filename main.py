cd248e4646cac7974208c17f6c4c5a64ec8bd8b5
premvkprakash <79302210+premvkprakash@users.noreply.github.com>
2022-02-03 18:38:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ from core import (
 
 REPO = """
 🤖 **Music Player**
-- Repo: [GitHub](https://github.com/ALBINPRAVEEN/MusicUserbot)
+- Repo: [GitHub](https://github.com/premvkprakash/MusicUserbot)
 - License: AGPL-3.0-or-later
 """
 
